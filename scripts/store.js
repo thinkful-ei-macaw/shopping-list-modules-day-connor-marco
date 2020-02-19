bc90d78317368109f8c5ffd0fbdb04eadbdebf31
Connor McCormack <mccormack.connor.m@gmail.com>
2020-02-19 11:22:21 -0600

    finished assignment

--- scripts/store.js
+++ scripts/store.js
@@ -3,14 +3,16 @@ import item from './item.js';
 const items = [];
 const hideCheckedItems = false;
 
+// returns object
 function findById(id) {
-  return items.find(element => element.id === id);
+  // by referring to items as this.items, we are referring to the exported object
+  return this.items.find(element => element.id === id);
 }
 
 function addItem(name) {
   try {
     item.validateName(name);
-    items.push(item.create(name));
+    this.items.push(item.create(name));
   } catch(error) {
     console.log(`Cannot add item: ${error.message}`);
   }
@@ -18,12 +20,38 @@ function addItem(name) {
 
 function findAndToggleChecked(id) {
   // find item by id
-  
+  let targetedObject = this.findById(id);
   // toggle checked attribute
+  targetedObject.checked = !targetedObject.checked;
 }
+
+function findAndUpdateName(id, newName) {
+  try {
+    item.validateName(newName);
+    let targetedObject = this.findById(id);
+    targetedObject.name = newName;
+  } catch(error) {
+    console.log(`Cannot update name: ${error.message}`)
+  }
+}
+
+function findAndDelete(id) {
+  // let targetedObject = findById(id);
+  this.items = this.items.filter(element => this.findById(id) !== element);
+}
+
+function toggleCheckedFilter() {
+  this.hideCheckedItems = !this.hideCheckedItems;
+};
 
 export default {
   items,
-  hideCheckedItems
+  hideCheckedItems,
+  findById,
+  addItem,
+  findAndToggleChecked,
+  findAndUpdateName,
+  findAndDelete,
+  toggleCheckedFilter
 };
 

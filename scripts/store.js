bfd03f182f9ef64916bbb38ed304e614a73491a0
Connor McCormack <mccormack.connor.m@gmail.com>
2020-02-19 09:33:25 -0600

    modified JS

--- scripts/store.js
+++ scripts/store.js
@@ -4,7 +4,22 @@ const items = [];
 const hideCheckedItems = false;
 
 function findById(id) {
-  return store.items.find(element => element.id === id);
+  return items.find(element => element.id === id);
+}
+
+function addItem(name) {
+  try {
+    item.validateName(name);
+    items.push(item.create(name));
+  } catch(error) {
+    console.log(`Cannot add item: ${error.message}`);
+  }
+}
+
+function findAndToggleChecked(id) {
+  // find item by id
+  
+  // toggle checked attribute
 }
 
 export default {
